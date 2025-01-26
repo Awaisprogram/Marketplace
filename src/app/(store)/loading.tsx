e0c42d959356cdbc941a6863cc954c7e9389616f
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-01-27 00:11:19 +0500

    Update loading.tsx

--- src/app/(store)/loading.tsx
+++ src/app/(store)/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading  ()  {
   return (
-    <div className="flex h-screen items-center justify-center bg-black ">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-[#23A6F0] border-t-transparent"></div>
+    <div className="flex h-screen items-center justify-center gap-2 bg-white ">
+      
+      <div className="h-8 w-8 animate-spin rounded-full border-4 border-solid border-[#23A6F0] border-t-transparent"></div>
+      <div>Bandage is loading...</div>
     </div>
   );
 };

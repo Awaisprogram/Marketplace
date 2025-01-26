ba4e861679ae4c2607626cebc30ff1b05092e10c
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-01-26 23:49:50 +0500

    Create loading.tsx

---
+++ src/app/(store)/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading  ()  {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black ">
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-[#23A6F0] border-t-transparent"></div>
+    </div>
+  );
+};
+

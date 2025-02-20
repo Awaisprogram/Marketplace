32e32c3002d5a12ae849d033e86d002131cf9e13
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2025-02-21 03:37:39 +0500

    Update loading.tsx

--- src/app/(store)/loading.tsx
+++ src/app/(store)/loading.tsx
@@ -1,10 +1,8 @@
+import LoadingSkeleton from '../components/Loading'
 export default function Loading  ()  {
   return (
-    <div className="flex h-screen items-center justify-center gap-2 bg-white ">
-      
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-solid border-[#23A6F0] border-t-transparent"></div>
-      <div>Bandage is loading...</div>
-    </div>
+    
+    <LoadingSkeleton/>
   );
 };
 

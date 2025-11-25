d79324eb7717c9224c46bdd2a63dcef9584b47fe
Awaisprogram <awaisbinmehmoodahmed@gmail.com>
2025-11-25 05:49:37 +0500

    solving error

--- src/app/components/searchbar.tsx
+++ src/app/components/searchbar.tsx
@@ -144,11 +144,9 @@ const SearchBar = () => {
                   Try another search term.
                 </p>
               ) : (
-<<<<<<< HEAD
-                <p className="flex gap-2">
-=======
+
                 <p className="flex items-center gap-2">
->>>>>>> acb118f0dd5675372f4c2c22c1f15babd6624e13
+
                   <Search className="w-5 h-5 text-gray-500" />
                   Start searching to see products.
                 </p>
